f84b5449488a91be4bfbaa71d32d96a9f3171d5b
Kevin Huang <kehuang805@gmail.com>
2021-04-01 10:25:46 -0700

    Job model and unit test written

--- models/job.js
+++ models/job.js
@@ -14,7 +14,7 @@ class Job {
     INSERT INTO jobs
     (title, salary, equity, company_handle)
     VALUES ($1, $2, $3, $4)
-    RETURNING title, salary, equity, company_handle AS "companyHandle"`,
+    RETURNING id, title, salary, equity, company_handle AS "companyHandle"`,
       [title, salary, equity, companyHandle]
     );
 
@@ -23,19 +23,37 @@ class Job {
     return job;
   }
 
+  /** Gets all jobs.
+   *
+   * Returns [{id, title, salary, equity, companyHandle}, ...]
+   */
   static async findAll() {
     const jobsRes = await db.query(
       `SELECT id, title, salary, equity, company_handle AS "companyHandle"
-      FROM jobs 
+      FROM jobs
       ORDER BY id`
     );
 
     return jobsRes.rows;
   }
 
+  /** Finds jobs that meet search parameters
+   * Throws error if not title, salary, or equity
+   * Returns [{id, title, salary, equity, companyHandle}, ...]
+   */
   static async filter(params) {
     Job.checkCriteria(params);
     let { statement, values } = Job.createSearchSqlStatement(params);
+
+    const querySql = `SELECT id,
+                             title,
+                             salary,
+                             equity,
+                             company_handle AS "companyHandle"
+                      FROM jobs
+                      WHERE ${statement}`
+    const result = await db.query(querySql, values);
+    return result.rows;
   }
 
   /** HELPER FUNCTION FOR FILTER
@@ -55,12 +73,19 @@ class Job {
     }
     const { hasEquity } = params;
     if (hasEquity) {
-      if (hasEquity !== "true" || hasEquity !== "false") {
+      if (hasEquity !== "true" && hasEquity !== "false") {
         throw new BadRequestError("Invalid value for has equity");
       }
     }
   }
 
+  /** HELPER FUNCTION FOR FILTER
+   *
+   * Creates template for SQL query to search jobs with valid filter
+   *
+   * Returns {statement: "column = $1 AND column = $2", values: [values, ...] }
+   *
+   */
   static createSearchSqlStatement(params) {
     const { title, minSalary, hasEquity } = params;
     let sqlStatement = [];
@@ -74,8 +99,8 @@ class Job {
       sqlStatement.push(`salary >= $${sqlStatement.length + 1}`);
       sqlValues.push(parseInt(minSalary));
     }
-    if (hasEquity === "true") {
-      sqlStatement.push(`equity >= $${sqlStatement.length + 1}`);
+    if (hasEquity === "true"  ) {
+      sqlStatement.push(`equity > $${sqlStatement.length + 1}`);
       sqlValues.push(`0`);
     }
 
@@ -83,4 +108,72 @@ class Job {
 
     return { statement: outStatement, values: sqlValues };
   }
+
+  /** Given a job id, return data about job
+   *
+   * Returns {id, title, salary, equity, companyHandle}
+   *
+   * Throws NotFoundError if not found.
+   */
+  static async get(id) {
+    const result = await db.query(
+        `SELECT id, title, salary, equity, company_handle AS "companyHandle"
+            FROM jobs
+            WHERE id = $1`,
+          [id]
+    );
+
+    const job = result.rows[0];
+
+    if (!job) throw new NotFoundError(`No job with id: ${id}.`);
+
+    return job;
+
+  }
+
+  /** Update job data with `data`
+   *
+   * Can be partial update
+   *
+   * Data can include: {title, salary, equity}
+   *
+   * Returns {id, title, salary, equity, companyHandle}
+   *
+   * Throws NotFoundError if not found.
+   */
+  static async update(id, data) {
+    const {setCols, values } = sqlForPartialUpdate(data);
+    const idVarIdx = "$" + (values.length + 1);
+    const querySql = `UPDATE jobs
+                        SET ${setCols}
+                        WHERE id = ${idVarIdx}
+                        RETURNING id,
+                                  title,
+                                  salary,
+                                  equity,
+                                  company_handle AS "companyHandle"`
+    const result = await db.query(querySql, [...values, id]);
+    const job = result.rows[0];
+
+    if(!job) throw new NotFoundError(`No job with id: ${id}.`);
+    return job;
+  }
+
+  /** Delete given job from database; returns undefined
+   *
+   * Throws NotFoundError if company not found.
+  */
+  static async remove(id) {
+    const result = await db.query(
+          `DELETE
+          FROM jobs
+          WHERE id = $1
+          RETURNING id`,
+          [id]);
+    const job = result.rows[0]
+
+    if(!job) throw new NotFoundError(`No job with id: ${id}.`);
+  }
 }
+
+module.exports = Job;
---
+++ models/job.test.js
@@ -0,0 +1,133 @@
+"use strict";
+
+const db = require("../db.js");
+const { BadRequestError, NotFoundError } = require("../expressError");
+const Job = require("./job.js");
+const {
+  commonBeforeAll,
+  commonBeforeEach,
+  commonAfterEach,
+  commonAfterAll,
+} = require("./_testCommon");
+
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
+
+/************************************** create */
+
+describe("create a job", function () {
+  const newJob = {
+    title: "new",
+    salary: 11,
+    equity: 0.01,
+    companyHandle: 'c1'
+  };
+
+  test("creating a job works", async function () {
+    let job = await Job.create(newJob);
+    expect(job).toEqual({
+      id: expect.any(Number),
+      title: "new",
+      salary: 11,
+      equity: "0.01",
+      companyHandle: 'c1'
+    });
+
+    const result = await db.query(
+      `SELECT id, title, salary, equity, company_handle AS "companyHandle"
+             FROM jobs
+             WHERE title = 'new'`);
+    expect(result.rows).toEqual([
+      {
+        id: expect.any(Number),
+        title: "new",
+        salary: 11,
+        equity: "0.01",
+        companyHandle: 'c1'
+      },
+    ]);
+  });
+
+
+});
+
+/************************************** findAll */
+
+describe("findAll jobs", function () {
+  test("findAll jobs works: no filter", async function () {
+    let jobs = await Job.findAll();
+    expect(jobs).toEqual([
+      {
+        id: expect.any(Number),
+        title: "j1",
+        equity: null,
+        salary: 1,
+        companyHandle: "c1",
+      },
+      {
+        id: expect.any(Number),
+        title: "j2",
+        equity: "0",
+        salary: 2,
+        companyHandle: "c2",
+      },
+      {
+        id: expect.any(Number),
+        title: "j3",
+        equity: "0.003",
+        salary: 3,
+        companyHandle: "c3",
+      },
+    ]);
+  });
+});
+
+/************************************** filter */
+
+describe("filter jobs", function () {
+  test("find jobs that match one query parameter successfully", async function () {
+    let jobs = await Job.filter({ minSalary: 3 });
+    expect(jobs.length).toEqual(1);
+    expect(jobs[0]).toEqual({
+      id: expect.any(Number),
+      title: "j3",
+      equity: "0.003",
+      salary: 3,
+      companyHandle: "c3"
+    });
+  });
+
+  test("find jobs that match multiple query params successfully", async function () {
+    let jobs = await Job.filter({ minSalary: 2, title: "j2" });
+    expect(jobs.length).toEqual(1);
+    expect(jobs[0]).toEqual({
+      id: expect.any(Number),
+      title: "j2",
+      equity: "0",
+      salary: 2,
+      companyHandle: "c2"
+    });
+  });
+
+  test("find jobs on equity filter properly", async function () {
+    let jobs = await Job.filter({ hasEquity: "true" });
+    expect(jobs.length).toEqual(1);
+    expect(jobs[0]).toEqual({
+      id: expect.any(Number),
+      title: "j3",
+      equity: "0.003",
+      salary: 3,
+      companyHandle: "c3"
+    })
+  });
+
+  test("bad request if incorrect parameter given", async function () {
+    try {
+      await Job.filter({ coffeeMachines: 2, title: "j2" });
+    } catch (error) {
+      expect(error instanceof BadRequestError).toBeTruthy();
+    }
+  });
+});
